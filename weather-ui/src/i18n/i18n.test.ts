586f20ea3c3f1fa2a5c79a28023ea5d3670178f4
Joshua Eaton <jeaton446@gmail.com>
2024-07-09 14:56:53 +0100

    add: Added unit testing for i18n translations

--- weather-ui/src/i18n/i18n.test.ts
+++ weather-ui/src/i18n/i18n.test.ts
@@ -27,4 +27,124 @@ describe("i18n translation", () => {
 		const frTitle = i18n.t("header.title");
 		expect(frTitle).toBe("Interface météo");
 	});
+
+	it("should translate the sidebar title", () => {
+		i18n.changeLanguage("en");
+		const enTitle = i18n.t("sidebar.title");
+		expect(enTitle).toBe("Recent Searches");
+		i18n.changeLanguage("es");
+		const esTitle = i18n.t("sidebar.title");
+		expect(esTitle).toBe("Búsquedas recientes");
+		i18n.changeLanguage("fr");
+		const frTitle = i18n.t("sidebar.title");
+		expect(frTitle).toBe("Recherches récentes");
+	});
+
+	it("should translate the weather title", () => {
+		i18n.changeLanguage("en");
+		const enTitle = i18n.t("weather.title");
+		expect(enTitle).toBe("Enter location");
+		i18n.changeLanguage("es");
+		const esTitle = i18n.t("weather.title");
+		expect(esTitle).toBe("Introducir ubicación");
+		i18n.changeLanguage("fr");
+		const frTitle = i18n.t("weather.title");
+		expect(frTitle).toBe("Entrez l'emplacement");
+	});
+
+	it("should translate the weather search button text", () => {
+		i18n.changeLanguage("en");
+		const enTitle = i18n.t("weather.search");
+		expect(enTitle).toBe("Search");
+		i18n.changeLanguage("es");
+		const esTitle = i18n.t("weather.search");
+		expect(esTitle).toBe("Buscar");
+		i18n.changeLanguage("fr");
+		const frTitle = i18n.t("weather.search");
+		expect(frTitle).toBe("Chercher");
+	});
+
+	it("should translate the weather temperature text", () => {
+		i18n.changeLanguage("en");
+		const enTitle = i18n.t("weather.temperature");
+		expect(enTitle).toBe("Temperature range");
+		i18n.changeLanguage("es");
+		const esTitle = i18n.t("weather.temperature");
+		expect(esTitle).toBe("Rango de temperatura");
+		i18n.changeLanguage("fr");
+		const frTitle = i18n.t("weather.temperature");
+		expect(frTitle).toBe("Écart de température");
+	});
+
+	it("should translate the weather humidity text", () => {
+		i18n.changeLanguage("en");
+		const enTitle = i18n.t("weather.humidity");
+		expect(enTitle).toBe("Humidity");
+		i18n.changeLanguage("es");
+		const esTitle = i18n.t("weather.humidity");
+		expect(esTitle).toBe("Humedad");
+		i18n.changeLanguage("fr");
+		const frTitle = i18n.t("weather.humidity");
+		expect(frTitle).toBe("Humidité");
+	});
+
+	it("should translate the weather sunrise text", () => {
+		i18n.changeLanguage("en");
+		const enTitle = i18n.t("weather.sunrise");
+		expect(enTitle).toBe("Sunrise");
+		i18n.changeLanguage("es");
+		const esTitle = i18n.t("weather.sunrise");
+		expect(esTitle).toBe("Amanecer");
+		i18n.changeLanguage("fr");
+		const frTitle = i18n.t("weather.sunrise");
+		expect(frTitle).toBe("Lever du soleil");
+	});
+
+	it("should translate the weather sunset text", () => {
+		i18n.changeLanguage("en");
+		const enTitle = i18n.t("weather.sunset");
+		expect(enTitle).toBe("Sunset");
+		i18n.changeLanguage("es");
+		const esTitle = i18n.t("weather.sunset");
+		expect(esTitle).toBe("Atardecer");
+		i18n.changeLanguage("fr");
+		const frTitle = i18n.t("weather.sunset");
+		expect(frTitle).toBe("Coucher de soleil");
+	});
+
+	it("should translate the weather errors fetching data text", () => {
+		i18n.changeLanguage("en");
+		const enTitle = i18n.t("weather.errors.fetchingData");
+		expect(enTitle).toBe("Error fetching data");
+		i18n.changeLanguage("es");
+		const esTitle = i18n.t("weather.errors.fetchingData");
+		expect(esTitle).toBe("Error al obtener datos");
+		i18n.changeLanguage("fr");
+		const frTitle = i18n.t("weather.errors.fetchingData");
+		expect(frTitle).toBe("Erreur lors de la récupération des données");
+	});
+
+	it("should translate the weather errors unable to fetch data text", () => {
+		i18n.changeLanguage("en");
+		const enTitle = i18n.t("weather.errors.unableToFetchData");
+		expect(enTitle).toBe("Unable to fetch data from the API");
+		i18n.changeLanguage("es");
+		const esTitle = i18n.t("weather.errors.unableToFetchData");
+		expect(esTitle).toBe("No se pueden obtener datos de la API");
+		i18n.changeLanguage("fr");
+		const frTitle = i18n.t("weather.errors.unableToFetchData");
+		expect(frTitle).toBe("Impossible de récupérer les données de l'API");
+	});
+
+	it("should translate the weather errors unable to find location text", () => {
+		i18n.changeLanguage("en");
+		const enTitle = i18n.t("weather.errors.unableToFindLocation");
+		expect(enTitle).toBe("Unable to search location. Please check spelling and try again.");
+		i18n.changeLanguage("es");
+		const esTitle = i18n.t("weather.errors.unableToFindLocation");
+		expect(esTitle).toBe("No se puede buscar la ubicación. Verifique la ortografía y vuelva a intentarlo.");
+		i18n.changeLanguage("fr");
+		const frTitle = i18n.t("weather.errors.unableToFindLocation");
+		expect(frTitle).toBe("Impossible de rechercher l'emplacement. Veuillez vérifier l'orthographe et réessayer.");
+	});
 });
